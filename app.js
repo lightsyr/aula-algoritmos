c3700409e8ba8a2d2309327a7a003b6b1568eb56
Leandro Campos <leandro.campos@gmail.com>
2025-08-06 19:37:13 -0300

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,14 @@
+class Personagem{
+    constructor(classe){
+        this.classe = classe
+    }
+}
+
+function escolheClasse(classe){
+    if(classe = "guerreiro"){
+        return new Personagem(classe)
+    }
+}
+
+let personagem1 = escolheClasse("guerreiro")
+console.log(personagem1)
